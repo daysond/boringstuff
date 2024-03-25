14b93ebed9ffc5ad0ebb2f9bf62108883c1441cc
Kannav Sethi <kannavsethi2@gmail.com>
2024-03-24 20:35:10 -0400

    main file

--- linux/main.cpp
+++ linux/main.cpp
@@ -10,12 +10,16 @@ using namespace std;
 int main() {
     DBTesting dbTesting;
 
-    // Testing memory leaks for AVL Tree and std::map
-    std::cout << "\nTesting Memory Leak for AVL Tree...\n";
-    dbTesting.test_memory_leak(DBTesting::TestType::AVLTREE);
+
 
     std::cout << "\nTesting Memory Leak for std::map...\n";
     dbTesting.test_memory_leak(DBTesting::TestType::MAP);
+
+
+    std::cout << "\nTesting Memory Leak for std::map...\n";
+    dbTesting.test_speed_search(DBTesting::TestType::MAP);
+
+
 
     return 0;
 }
